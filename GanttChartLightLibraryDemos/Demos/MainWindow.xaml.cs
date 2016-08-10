8c21789743b93e49c65cc7e9bbc46af131918966
DlhSoft Team <Contact@DlhSoft.com>
2016-08-10 18:30:28 +0300

    Expanders

--- GanttChartLightLibraryDemos/Demos/MainWindow.xaml.cs
+++ GanttChartLightLibraryDemos/Demos/MainWindow.xaml.cs
@@ -31,5 +31,44 @@ namespace Demos
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
         }
+
+        private void TreeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var firstItem = TreeView.Items[0] as TreeViewItem;
+            firstItem.IsSelected = true;
+        }
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var selectedItem = e.NewValue as TreeViewItem;
+            if (selectedItem.HasItems)
+            {
+                var firstChildItem = selectedItem.Items[0] as TreeViewItem;
+                firstChildItem.IsSelected = true;
+                selectedItem.IsExpanded = true;
+            }
+            else
+            {
+                if (previousSelectedParentItem != null)
+                    previousSelectedParentItem.Style = previousSelectedParentItemStyle;
+                var parentItem = selectedItem.Parent as TreeViewItem;
+                previousSelectedParentItem = parentItem;
+                previousSelectedParentItemStyle = parentItem.Style;
+                parentItem.Style = Resources["SelectedTreeViewItemParentStyle"] as Style;
+            }
+        }
+        private TreeViewItem previousSelectedParentItem;
+        private Style previousSelectedParentItemStyle;
+        private void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            var expandedItem = sender as TreeViewItem;
+            if (!expandedItem.HasItems || !expandedItem.IsExpanded)
+                return;
+            expandedItem.IsSelected = true;
+            foreach (TreeViewItem item in TreeView.Items)
+            {
+                if (item.HasItems && item.IsExpanded && item != expandedItem)
+                    item.IsExpanded = false;
+            }
+        }
     }
 }
