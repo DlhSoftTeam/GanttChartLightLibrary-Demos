efb1e89899a55bb42268f1c80a96dc08bd918776
DlhSoft Team <Contact@DlhSoft.com>
2018-06-28 16:29:33 +0300

    Added dependency line templating sample

---
+++ GanttChartLightLibraryDemos/Demos/Samples/WPF-CSharp/GanttChartDataGrid/DependencyLineTemplating/PredecessorItemExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Demos.WPF.CSharp.GanttChartDataGrid.DependencyLineTemplating
+{
+    public static class PredecessorItemExtensions
+    {
+        public static readonly DependencyProperty IsImportantProperty = DependencyProperty.RegisterAttached("IsImportant", typeof(bool), typeof(PredecessorItemExtensions));
+        public static bool GetIsImportant(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsImportantProperty);
+        }
+        public static void SetIsImportant(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsImportantProperty, value);
+        }
+    }
+}
