63a586d8813ff430c56fa5649e08f63d630ab6cf
DlhSoft Team <Contact@DlhSoft.com>
2019-04-01 22:35:19 +0300

    Updated packages - DataBinding demo fixes

--- GanttChartLightLibraryDemos/Demos/Properties/AssemblyInfo.cs
+++ GanttChartLightLibraryDemos/Demos/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DlhSoft")]
 [assembly: AssemblyProduct("Demos")]
-[assembly: AssemblyCopyright("Copyright © DlhSoft 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © DlhSoft 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
