2c1e2a732121a484c4b5c3c9a2ac7e19b98c2d27
DlhSoft Team <Contact@DlhSoft.com>
2020-05-29 19:41:26 +0300

    Fixes for SummaryBars sample

--- GanttChartLightLibraryDemos/Demos/Samples/WPF-CSharp/GanttChartDataGrid/SummaryBars/MainWindow.xaml.cs
+++ GanttChartLightLibraryDemos/Demos/Samples/WPF-CSharp/GanttChartDataGrid/SummaryBars/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using DlhSoft.Windows.Controls;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace Demos.WPF.CSharp.GanttChartDataGrid.SummaryBars
 {
@@ -64,26 +66,32 @@ namespace Demos.WPF.CSharp.GanttChartDataGrid.SummaryBars
                     if (item.HasChildren)
                     {
                         // Store children of each summary task for reference purposes.
-                        item.Tag = GanttChartDataGrid.GetAllChildren(item);
+                        item.Tag = GanttChartDataGrid.GetAllChildren(item).ToArray();
                         continue;
                     }
                     GanttChartItem clone = new GanttChartItem();
                     BindingOperations.SetBinding(clone, GanttChartItem.ContentProperty, new Binding("Content") { Source = item });
-                    BindingOperations.SetBinding(clone, GanttChartItem.StartProperty, new Binding("Start") { Source = item });
-                    BindingOperations.SetBinding(clone, GanttChartItem.FinishProperty, new Binding("Finish") { Source = item });
-                    BindingOperations.SetBinding(clone, GanttChartItem.CompletedFinishProperty, new Binding("CompletedFinish") { Source = item });
-                    BindingOperations.SetBinding(clone, GanttChartItem.IsMilestoneProperty, new Binding("IsMilestone") { Source = item });
+                    BindingOperations.SetBinding(clone, GanttChartItem.StartProperty, new Binding("Start") { Source = item, Mode = BindingMode.OneTime });
+                    BindingOperations.SetBinding(clone, GanttChartItem.FinishProperty, new Binding("Finish") { Source = item, Mode = BindingMode.OneTime });
+                    BindingOperations.SetBinding(clone, GanttChartItem.CompletedFinishProperty, new Binding("CompletedFinish") { Source = item, Mode = BindingMode.OneTime });
+                    BindingOperations.SetBinding(clone, GanttChartItem.IsMilestoneProperty, new Binding("IsMilestone") { Source = item, Mode = BindingMode.OneTime });
                     BindingOperations.SetBinding(clone, GanttChartItem.AssignmentsContentProperty, new Binding("AssignmentsContent") { Source = item });
                     BindingOperations.SetBinding(clone, GanttChartItem.DisplayRowIndexProperty, new Binding("ActualDisplayRowIndex") { Source = item });
                     BindingOperations.SetBinding(clone, GanttChartItem.IsVisibleProperty, new Binding("IsVisible") { Source = item });
-                    BindingOperations.SetBinding(item, GanttChartItem.StartProperty, new Binding("Start") { Source = clone });
-                    BindingOperations.SetBinding(item, GanttChartItem.FinishProperty, new Binding("Finish") { Source = clone });
-                    BindingOperations.SetBinding(item, GanttChartItem.CompletedFinishProperty, new Binding("CompletedFinish") { Source = clone });
-                    BindingOperations.SetBinding(item, GanttChartItem.IsMilestoneProperty, new Binding("IsMilestone") { Source = clone });
+                    startPropertyDescriptor.AddValueChanged(clone, CloneStartChanged);
+                    startPropertyDescriptor.AddValueChanged(item, ItemStartChanged);
+                    if (!item.IsMilestone)
+                    {
+                        finishPropertyDescriptor.AddValueChanged(clone, CloneFinishChanged);
+                        finishPropertyDescriptor.AddValueChanged(item, ItemFinishChanged);
+                        completedFinishPropertyDescriptor.AddValueChanged(clone, CloneCompletedFinishChanged);
+                        completedFinishPropertyDescriptor.AddValueChanged(item, ItemCompletedFinishChanged);
+                    }
+                    isMilestonePropertyDescriptor.AddValueChanged(item, ItemIsMilestoneChanged);
                     // Store clones as item tags for reference purposes.
                     item.Tag = clone;
                     // Store parents of each cloned item for reference purposes.
-                    clone.Tag = GanttChartDataGrid.GetAllParents(item);
+                    clone.Tag = new ItemHierarchyInfo { Item = item, ParentItems = GanttChartDataGrid.GetAllParents(item).ToArray() };
                     ganttChartItemClones.Add(clone);
                 }
                 GanttChartDataGrid.GanttChartView.Items = ganttChartItemClones;
@@ -119,6 +127,95 @@ namespace Demos.WPF.CSharp.GanttChartDataGrid.SummaryBars
                 return;
             var themeResourceDictionary = new ResourceDictionary { Source = new Uri("/" + GetType().Assembly.GetName().Name + ";component/Themes/" + theme + ".xaml", UriKind.Relative) };
             GanttChartDataGrid.Resources.MergedDictionaries.Add(themeResourceDictionary);
+        }
+
+
+        // Miscellaneous helpers.
+        private DependencyPropertyDescriptor startPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(GanttChartItem.StartProperty, typeof(GanttChartItem));
+        private DependencyPropertyDescriptor finishPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(GanttChartItem.FinishProperty, typeof(GanttChartItem));
+        private DependencyPropertyDescriptor completedFinishPropertyDescriptor = DependencyPropertyDescriptor.FromProperty(GanttChartItem.CompletedFinishProperty, typeof(GanttChartItem));
+        private DependencyPropertyDescriptor isMilestonePropertyDescriptor = DependencyPropertyDescriptor.FromProperty(GanttChartItem.IsMilestoneProperty, typeof(GanttChartItem));
+        private void CloneStartChanged(object sender, EventArgs e)
+        {
+            var clone = sender as GanttChartItem;
+            var hierarchyInfo = clone.Tag as ItemHierarchyInfo;
+            var item = hierarchyInfo.Item;
+            if (clone.Start != item.Start)
+                item.SetCurrentValue(GanttChartItem.StartProperty, clone.Start);
+        }
+        private void ItemStartChanged(object sender, EventArgs e)
+        {
+            var item = sender as GanttChartItem;
+            var clone = item.Tag as GanttChartItem;
+            if (item.Start != clone.Start)
+                clone.SetCurrentValue(GanttChartItem.StartProperty, item.Start);
+        }
+        private void CloneFinishChanged(object sender, EventArgs e)
+        {
+            var clone = sender as GanttChartItem;
+            var hierarchyInfo = clone.Tag as ItemHierarchyInfo;
+            var item = hierarchyInfo.Item;
+            if (clone.Finish != item.Finish)
+                item.SetCurrentValue(GanttChartItem.FinishProperty, clone.Finish);
+        }
+        private void ItemFinishChanged(object sender, EventArgs e)
+        {
+            var item = sender as GanttChartItem;
+            var clone = item.Tag as GanttChartItem;
+            if (item.Finish != clone.Finish)
+                clone.SetCurrentValue(GanttChartItem.FinishProperty, item.Finish);
+        }
+        private void CloneCompletedFinishChanged(object sender, EventArgs e)
+        {
+            var clone = sender as GanttChartItem;
+            var hierarchyInfo = clone.Tag as ItemHierarchyInfo;
+            var item = hierarchyInfo.Item;
+            if (clone.Finish != item.Finish)
+                item.SetCurrentValue(GanttChartItem.FinishProperty, clone.Finish);
+            if (clone.CompletedFinish != item.CompletedFinish)
+                item.SetCurrentValue(GanttChartItem.CompletedFinishProperty, clone.CompletedFinish);
+        }
+        private void ItemCompletedFinishChanged(object sender, EventArgs e)
+        {
+            ItemFinishChanged(sender, EventArgs.Empty);
+            var item = sender as GanttChartItem;
+            var clone = item.Tag as GanttChartItem;
+            if (item.Finish != clone.Finish)
+                clone.SetCurrentValue(GanttChartItem.FinishProperty, item.Finish);
+            if (item.CompletedFinish != clone.CompletedFinish)
+                clone.SetCurrentValue(GanttChartItem.CompletedFinishProperty, item.CompletedFinish);
+        }
+        private void ItemIsMilestoneChanged(object sender, EventArgs e)
+        {
+            var item = sender as GanttChartItem;
+            var clone = item.Tag as GanttChartItem;
+            if (!item.IsMilestone)
+            {
+                clone.SetCurrentValue(GanttChartItem.StartProperty, item.Start);
+                clone.SetCurrentValue(GanttChartItem.FinishProperty, item.Finish);
+                clone.SetCurrentValue(GanttChartItem.CompletedFinishProperty, item.CompletedFinish);
+                finishPropertyDescriptor.AddValueChanged(clone, CloneFinishChanged);
+                finishPropertyDescriptor.AddValueChanged(item, ItemFinishChanged);
+                completedFinishPropertyDescriptor.AddValueChanged(clone, CloneCompletedFinishChanged);
+                completedFinishPropertyDescriptor.AddValueChanged(item, ItemCompletedFinishChanged);
+            }
+            else
+            {
+                finishPropertyDescriptor.RemoveValueChanged(clone, CloneFinishChanged);
+                finishPropertyDescriptor.RemoveValueChanged(item, ItemFinishChanged);
+                completedFinishPropertyDescriptor.RemoveValueChanged(clone, CloneCompletedFinishChanged);
+                completedFinishPropertyDescriptor.RemoveValueChanged(item, ItemCompletedFinishChanged);
+                item.SetCurrentValue(GanttChartItem.StartProperty, clone.Start);
+                item.SetCurrentValue(GanttChartItem.FinishProperty, clone.Finish);
+                item.SetCurrentValue(GanttChartItem.CompletedFinishProperty, clone.CompletedFinish);
+            }
+            if (item.IsMilestone != clone.IsMilestone)
+                clone.SetCurrentValue(GanttChartItem.IsMilestoneProperty, item.IsMilestone);
+        }
+        private class ItemHierarchyInfo
+        {
+            public GanttChartItem Item { get; set; }
+            public IEnumerable<GanttChartItem> ParentItems { get; set; }
         }
 
         // Stores the clones of leaf Gantt Chart items.
